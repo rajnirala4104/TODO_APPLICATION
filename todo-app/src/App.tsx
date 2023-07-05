8bdb8368c4d9a062d268c2fa795687b96c30de5e
raj nirala <rajnirala4104@gmail.com>
2023-07-05 15:10:23 +0530

    setup react and tailiwand css

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="text-red-600">This is Todo Application</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
