8dedfe85a20f40d9a92d6f969cc4111c7265cfa4
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-09-28 13:13:30 +0530

    removed something

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,6 @@ const controllers = require("./controller/userController");
 
 const app = express();
 
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-   next();
-});
-
 app.get("/api", controllers.showUsers);
 app.get("/api/user/:userName", controllers.userInfo);
 
